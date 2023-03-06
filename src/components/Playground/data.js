41f1e41689a319295832e5c2be5686919d966b2c
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2023-03-06 18:08:18 +0900

    Playground Component Updated

--- src/components/Playground/data.js
+++ src/components/Playground/data.js
@@ -1,35 +1,51 @@
-import dribble from "../../assets/playground/dribble.png";
-import random from "../../assets/playground/random.png";
+import project1 from "../../assets/playground/project-1.png";
+import project2 from "../../assets/playground/project-2.png";
 import countdown from "../../assets/playground/countdown.png";
-import awwwards from "../../assets/playground/awwwards.png";
+import project3 from "../../assets/playground/project-3.png";
+import project4 from "../../assets/playground/project-4.png";
+import project5 from "../../assets/playground/project-5.png";
 
 export const projectsData = [
-  {
-    id: 1,
-    projectName: "Dribble Homepage Clone",
-    liveLink: "https://dribble-clone.netlify.app/",
-    hasLink: true,
-    img: dribble,
-  },
-  {
-    id: 2,
-    projectName: "Random Quote Generator",
-    liveLink: "https://js-project-quote-generator.netlify.app/",
-    hasLink: true,
-    img: random,
-  },
-  {
-    id: 3,
-    projectName: "New Year Countdown",
-    hasLink: false,
-    liveLink: "",
-    img: countdown,
-  },
-  {
-    id: 4,
-    projectName: "Awwwards Clone",
-    hasLink: false,
-    liveLink: "",
-    img: awwwards,
-  },
+    {
+        id: 1,
+        projectName: "Electric Car Website",
+        liveLink: "https://electric-car-website.netlify.app/",
+        hasLink: true,
+        img: project1,
+    },
+    {
+        id: 2,
+        projectName: "Social Media Website",
+        liveLink: "https://social-media-html-css-js.netlify.app/",
+        hasLink: true,
+        img: project2,
+    },
+    {
+        id: 3,
+        projectName: "New Year Countdown",
+        hasLink: false,
+        liveLink: "",
+        img: countdown,
+    },
+    {
+        id: 4,
+        projectName: "Responsive Hospital Website",
+        hasLink: true,
+        liveLink: "https://responsive-hospital-websitee.netlify.app/",
+        img: project4,
+    },
+    {
+        id: 5,
+        projectName: "Admin Dashboard",
+        hasLink: true,
+        liveLink: "https://responsive-admin-dashboardd.netlify.app/",
+        img: project3,
+    },
+    {
+        id: 6,
+        projectName: "API Home Furniture Project",
+        hasLink: true,
+        liveLink: "https://javascript-basic-projects-20-products.netlify.app/",
+        img: project5,
+    },
 ];
